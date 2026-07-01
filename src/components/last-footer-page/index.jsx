f72ad586e6bc2f139c579b9529da93203f370eb2
avetisyanmarine <marineavet2006@gmail.com>
2026-07-01 18:40:34 +0400

    fix: instagram asset path case

--- src/components/last-footer-page/index.jsx
+++ src/components/last-footer-page/index.jsx
@@ -1,7 +1,6 @@
 import { Container, Flexible } from "../../GlobalStyle";
 import { LastFooterPagePart, LastFooterPagePartContext } from "./styled";
 import PhoneSvg from "../../assets/vectors/Phone.svg";
-import InstagramSvg from "../../assets/vectors/instagram.svg";
 
 export const LastFooterPage = () => {
   return (
@@ -10,7 +9,21 @@ export const LastFooterPage = () => {
         <LastFooterPagePartContext>
           <p>Հրավիրատոմսը պատրաստվել է</p>
           <Flexible className="items-center justify-center gap-2 mt-4!">
-            <img loading="lazy" src={InstagramSvg} width={21} alt="instagram" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.75"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+              <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+            </svg>
             <a
               href="https://www.instagram.com/webinvite.am"
               target="_blank"
